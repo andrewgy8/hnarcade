691f7cf158a693b0b407ea6640ae25465ae0ba08
Andrew Graham-Yooll <andrewgy8@gmail.com>
2026-01-28 08:58:43 +0100

    Update favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'HN Arcade',
   tagline: 'Discover games from Hacker News Show HN',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   future: {
     v4: true,
