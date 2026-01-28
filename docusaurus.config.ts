42522b2e5c0d66b7dc335cdd72f716c5fea3fe8b
Andrew Graham-Yooll <andrewgy8@gmail.com>
2026-01-28 11:18:46 +0100

    Add edit this page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
           routeBasePath: 'games',
           path: 'docs',
           tagsBasePath: 'tags',
+          editUrl: 'https://github.com/andrewgy8/hnarcade/edit/main/',
         },
         blog: false,
         theme: {
